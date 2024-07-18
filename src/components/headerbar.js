aa3ba31d6512d4301ab019167e3311ff7dd25a18
EGheska <dimanich300@gmail.com>
2024-07-18 20:14:10 +0100

    Header bar has been created with the initial styling for it

---
+++ src/components/headerbar.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "../styles/headerbar.css";
+
+const HeaderBar = () => {
+    return (
+        <div className="header">
+        <header className="header-bar">
+            <h1>HeaderBar</h1>
+        </header>
+        </div>
+    );
+}
+
+export default HeaderBar;
