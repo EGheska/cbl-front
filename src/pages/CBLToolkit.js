b9aeb8d7fee59ba84233277a10bddde7e6bf2cb0
EGheska <dimanich300@gmail.com>
2024-08-15 02:11:44 +0200

    Added initial layout of questionaire

--- src/pages/CBLToolkit.js
+++ src/pages/CBLToolkit.js
@@ -1,10 +1,21 @@
 import React from "react";
 import Linker from "../components/linker";
+import {Button} from "@mui/material";
 
 const CBLToolkit = ({items}) => {
     return (
         <div className="pageClass">
             <Linker items={items}/>
+            <div className="questionaire-container">
+                <div className="content-box-header">Community-Based Learning Helper</div>
+            </div>
+            <div className="questionaire-footer">
+                <Button variant="contained"
+
+                >
+                    Next
+                </Button>
+            </div>
         </div>
     )
 };
