89cb60f17715f15f3676527072848b132d098b44
EGheska <dimanich300@gmail.com>
2024-08-18 23:01:58 +0100

    Added styling for the results

--- src/pages/result.js
+++ src/pages/result.js
@@ -1,13 +1,13 @@
 import React from "react";
 import {Button} from "@mui/material";
+import "../styles/results.css";
 const Result = () => {
     return (
         <div>
-            <div className="PageClass">
-                {/*<h1>Result Page</h1>*/}
+            <div className="pageClass-result">
+                <p>Community Based Learning Helper</p>
                 <div className="content-box">
                     <div className="content-box-header">
-                        <h1>Community-Based Learning Helper</h1>
                         <div className="emptyLine"></div>
                         <p>These are the results based on your answers</p>
                     </div>
